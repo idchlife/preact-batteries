40120eb53fc13c71c1f30a7c6c1151e95efad18a
idchlife <idchlife@gmail.com>
2017-03-09 19:47:50 +0300

    Fx

--- index.d.ts
+++
@@ -1,2 +0,0 @@
-export { AuthDependentComponent, AuthenticationStoreInterface } from "./src/component-modules/auth-dependent-component";
-export { modules, CompatibleComponentInterface, ComponentModule } from "./src/component-modules/module-system";
