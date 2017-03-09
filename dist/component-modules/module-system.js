7391daf6ecd940f0ce7114a34f40b2339e0c3d08
idchlife <idchlife@gmail.com>
2017-03-10 00:20:36 +0300

    Fx

--- dist/component-modules/module-system.js
+++ dist/component-modules/module-system.js
@@ -1,17 +1,7 @@
-(function (factory) {
-    if (typeof module === "object" && typeof module.exports === "object") {
-        var v = factory(require, exports);
-        if (v !== undefined) module.exports = v;
-    }
-    else if (typeof define === "function" && define.amd) {
-        define(["require", "exports"], factory);
-    }
-})(function (require, exports) {
-    "use strict";
-    Object.defineProperty(exports, "__esModule", { value: true });
-    function modules(component, modules) {
-        if (modules === void 0) { modules = []; }
-        modules.forEach(function (m) { return m.init(component); });
-    }
-    exports.modules = modules;
-});
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function modules(component, modules) {
+    if (modules === void 0) { modules = []; }
+    modules.forEach(function (m) { return m.init(component); });
+}
+exports.modules = modules;
--- dist/index.js
+++ dist/index.js
@@ -1,18 +1,8 @@
-(function (factory) {
-    if (typeof module === "object" && typeof module.exports === "object") {
-        var v = factory(require, exports);
-        if (v !== undefined) module.exports = v;
-    }
-    else if (typeof define === "function" && define.amd) {
-        define(["require", "exports", "./component-modules/auth-dependent-component", "./component-modules/module-system", "./dispatchers/simple-dispatcher"], factory);
-    }
-})(function (require, exports) {
-    "use strict";
-    Object.defineProperty(exports, "__esModule", { value: true });
-    var auth_dependent_component_1 = require("./component-modules/auth-dependent-component");
-    exports.AuthDependentComponent = auth_dependent_component_1.AuthDependentComponent;
-    var module_system_1 = require("./component-modules/module-system");
-    exports.modules = module_system_1.modules;
-    var simple_dispatcher_1 = require("./dispatchers/simple-dispatcher");
-    exports.SimpleDispatcher = simple_dispatcher_1.default;
-});
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var auth_dependent_component_1 = require("./component-modules/auth-dependent-component");
+exports.AuthDependentComponent = auth_dependent_component_1.AuthDependentComponent;
+var module_system_1 = require("./component-modules/module-system");
+exports.modules = module_system_1.modules;
+var simple_dispatcher_1 = require("./dispatchers/simple-dispatcher");
+exports.SimpleDispatcher = simple_dispatcher_1.default;
