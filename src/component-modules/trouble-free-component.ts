59e70ac699dace279aa790b86d36482cc21a433a
idchlife <idchlife@gmail.com>
2017-03-26 15:10:08 +0300

    Renamed state to more complicated to avoid name collision

--- src/component-modules/trouble-free-component.ts
+++ src/component-modules/trouble-free-component.ts
@@ -69,7 +69,7 @@ function onAppearedErrorFunc(e: Error) {
   if (this.forceUpdate) {
     onErrorFunc.call(this, e);
 
-    this.setState({ error: Math.random() * 1000 });
+    this.setState({ __error_state_7: Math.random() * 1000 });
   } else {
     console.warn(
       `[preact-batteries.trouble-free-component]: component did not have forceUpdate method,
